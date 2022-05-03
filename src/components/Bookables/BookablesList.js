61b60b795c16dca4c87d35b85e2382d2a1b71b51
Tom Zhang <hlzhang26@outlook.com>
2022-05-03 10:31:26 -0600

    Set up hooks to manage states in BookableList component

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,13 +1,22 @@
 import { useState } from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 import data from '../../static';
-const { bookables } = data;
+const { bookables, sessions, days } = data;
 
 export default function BookablesList() {
   // Set the group of bookables to be shown.
-  const group = 'Rooms';
+  const [group, setGroup] = useState('Kit');
   const bookablesInGroup = bookables.filter((b) => b.group === group);
-  const [bookableIndex, setBookableIndex] = useState(1);
+  const [bookableIndex, setBookableIndex] = useState(0);
+  const groups = [...new Set(bookables.map((b) => b.group))];
+
+  const bookable = bookablesInGroup[bookableIndex];
+  const [hasDetails, setHasDetails] = useState(false);
+
+  function changeGroup(event) {
+    setGroup(event.target.value);
+    setBookableIndex(0);
+  }
 
   function nextBookable() {
     // Passing th updater function a function when the new state depends on the previous state
@@ -15,25 +24,74 @@ export default function BookablesList() {
   }
 
   return (
-    <div>
-      <select></select>
-      <ul className="bookables items-list-nav">
-        {bookablesInGroup.map((b, i) => {
-          return (
-            <li key={b.id} className={i === bookableIndex ? 'selected' : null}>
-              <button className="btn" onClick={() => setBookableIndex(i)}>
-                {b.title}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-      <p>
-        <button className="btn" onClick={nextBookable} autoFocus>
-          <FaArrowRight />
-          <span>Next</span>
-        </button>
-      </p>
-    </div>
+    <>
+      <div>
+        <select value={group} onChange={changeGroup}>
+          {groups.map((g) => (
+            <option value={g} key={g}>
+              {g}
+            </option>
+          ))}
+        </select>
+        <ul className="bookables items-list-nav">
+          {bookablesInGroup.map((b, i) => {
+            return (
+              <li
+                key={b.id}
+                className={i === bookableIndex ? 'selected' : null}
+              >
+                <button className="btn" onClick={() => setBookableIndex(i)}>
+                  {b.title}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <p>
+          <button className="btn" onClick={nextBookable} autoFocus>
+            <FaArrowRight />
+            <span>Next</span>
+          </button>
+        </p>
+      </div>
+      {bookable && (
+        <div className="bookable-details">
+          <div className="item">
+            <div className="item-header">
+              <h2>{bookable.title}</h2>
+              <span className="controls">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={hasDetails}
+                    onChange={() => setHasDetails((has) => !has)}
+                  />
+                  Show Details
+                </label>
+              </span>
+            </div>
+            <p>{bookable.notes}</p>
+
+            {hasDetails && (
+              <div className="item-details">
+                <h3>Availability</h3>
+                <div className="bookable-availability">
+                  <ul>
+                    {bookable.days.sort().map((d) => (
+                      <li key={d}>{days[d]}</li>
+                    ))}
+                  </ul>
+                  <ul>
+                    {bookable.sessions.map((s) => (
+                      <li key={s}>{sessions[s]}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -4,6 +4,7 @@ const { users } = data;
 
 export default function UsersList() {
   const [userIndex, setUserIndex] = useState(1);
+  const user = users[userIndex];
 
   function changeUser(selectedIndex) {
     setUserIndex(selectedIndex);
@@ -11,16 +12,29 @@ export default function UsersList() {
   }
 
   return (
-    <ul className="user items-list-nav">
-      {users.map((u, i) => {
-        return (
-          <li key={u.id} className={i === userIndex ? 'selected' : null}>
-            <button className="btn" onClick={() => changeUser(i)}>
-              {u.name}
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <ul className="users items-list-nav">
+        {users.map((u, i) => {
+          return (
+            <li key={u.id} className={i === userIndex ? 'selected' : null}>
+              <button className="btn" onClick={() => changeUser(i)}>
+                {u.name}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      {user && (
+        <div className="item user">
+          <div className="item-header">
+            <h2>{user.name}</h2>
+          </div>
+          <div className="item-details">
+            <h3>{user.title}</h3>
+            <p>{user.notes}</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
