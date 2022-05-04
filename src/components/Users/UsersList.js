fb0b35e47d8986ca836fd03241af4009528bd78c
Tom Zhang <hlzhang26@outlook.com>
2022-05-04 17:42:09 -0600

    Refatored UsersList to lift up state to parent UsersPage

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -2,27 +2,26 @@ import { useState, useEffect } from 'react';
 import PageSpinner from '../UI/PageSpinner';
 import getData from '../utils/api';
 
-export default function UsersList() {
+export default function UsersList({ user, setUser }) {
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [userIndex, setUserIndex] = useState(1);
-  const user = users ? users[userIndex] : null;
 
   useEffect(() => {
     getData('http://localhost:3001/users')
-      .then((data) => {
+      .then((users) => {
+        setUser(users[0]);
         setIsLoading(false);
-        setUsers(data);
+        setUsers(users);
       })
       .catch((error) => {
         setIsLoading(false);
         setError(error);
       });
-  }, []);
+  }, [setUser]);
 
-  function changeUser(selectedIndex) {
-    setUserIndex(selectedIndex);
+  function changeUser(selectedUser) {
+    setUser(selectedUser);
   }
 
   if (error) {
@@ -33,30 +32,17 @@ export default function UsersList() {
     return <PageSpinner />;
   }
 
-  return (
-    <>
-      <ul className="users items-list-nav">
-        {users.map((u, i) => {
-          return (
-            <li key={u.id} className={i === userIndex ? 'selected' : null}>
-              <button className="btn" onClick={() => changeUser(i)}>
-                {u.name}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-      {user && (
-        <div className="item user">
-          <div className="item-header">
-            <h2>{user.name}</h2>
-          </div>
-          <div className="item-details">
-            <h3>{user.title}</h3>
-            <p>{user.notes}</p>
-          </div>
-        </div>
-      )}
-    </>
-  );
+  return users && user ? (
+    <ul className="users items-list-nav">
+      {users.map((u, i) => {
+        return (
+          <li key={u.id} className={u.id === user.id ? 'selected' : null}>
+            <button className="btn" onClick={() => changeUser(u)}>
+              {u.name}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  ) : null;
 }
